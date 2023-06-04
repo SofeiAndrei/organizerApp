96a4348d93257c892cc646ba3503b11d6e617c7c
Sofei Andrei <andrei.sofei@s.unibuc.ro>
2023-06-04 16:58:10 +0300

    Styled the calendars

--- app/javascript/react_components/calendars/CalendarFilters.jsx
+++ app/javascript/react_components/calendars/CalendarFilters.jsx
@@ -115,12 +115,12 @@ const CalendarFilters = (props) => {
 
   return (
     <div className='filters-container'>
-      Filter Tasks and Events:
+      <h3>Filters:</h3>
       {props.teamCalendar ? (
         <div className='team-calendar-filters'>
           {props.availableFilters.team_projects.length > 0 && (
             <div>
-              Team Project:
+              <h4>Team Project:</h4>
               {props.availableFilters.team_projects.map((project, index) => (
                 <div className='filter'>
                   <input key={`project${index}`} type='checkbox' id={`project${index}`} value={JSON.stringify(project)} checked={tempFilters.team_projects.map(project => project.id).includes(project.id)} onChange={handleChangedCheckbox}/>
@@ -129,13 +129,15 @@ const CalendarFilters = (props) => {
               ))}
             </div>
           )}
-          Assignee:
-          {props.availableFilters.team_members.map((member, index) => (
-            <div className='filter'>
-              <input key={`member${index}`} type='checkbox' id={`member${index}`} value={JSON.stringify(member)} checked={tempFilters.team_members.map(member => member.id).includes(member.id)} onChange={handleChangedCheckbox}/>
-              <label htmlFor={`member${index}`}>{member.name}</label>
-            </div>
-          ))}
+          <div>
+            <h4>Assignee:</h4>
+            {props.availableFilters.team_members.map((member, index) => (
+              <div className='filter'>
+                <input key={`member${index}`} type='checkbox' id={`member${index}`} value={JSON.stringify(member)} checked={tempFilters.team_members.map(member => member.id).includes(member.id)} onChange={handleChangedCheckbox}/>
+                <label htmlFor={`member${index}`}>{member.name}</label>
+              </div>
+            ))}
+          </div>
         </div>
       ) : (
         <div className='personal-calendar-filters'>
@@ -145,14 +147,14 @@ const CalendarFilters = (props) => {
           </div>
           {props.availableFilters.teams.length > 0 && (
             <div>
-              Team:
+              <h4>Team:</h4>
               {props.availableFilters.teams.map((team, index) => (
                 <div className='filter'>
                   <input key={`team${index}`} type='checkbox' id={`team${index}`} value={JSON.stringify(team)} checked={tempFilters.teams.map(team => team.id).includes(team.id)} onChange={handleChangedCheckbox}/>
                   <label htmlFor={`team${index}`}>{team.name}</label>
                   {props.availableFilters.team_projects.filter(team_project => team_project.team_id === team.id).length > 0 &&
                     <div className='team-team-projects-filters'>
-                      Team Project:
+                      <h4>Team Project:</h4>
                       {props.availableFilters.team_projects.filter(team_project => team_project.team_id === team.id).map((project, index) => (
                         <div className='filter'>
                           <input key={`project${index}`} type='checkbox' id={`project${index}`} value={JSON.stringify(project)} checked={tempFilters.team_projects.map(project => project.id).includes(project.id)} onChange={handleChangedCheckbox}/>
